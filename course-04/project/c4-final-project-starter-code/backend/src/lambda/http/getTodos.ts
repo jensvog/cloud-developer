89d8bde7d42b226eba0c930c2cdcf3cb6201276f
jvog <62656589+jensvog@users.noreply.github.com>
2020-04-25 20:04:24 +0200

    Added first Todos
    * Authentification
    * Get Todos

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,35 @@
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+const todosTable = process.env.TODOS_TABLE
+const indexName = process.env.INDEX_NAME
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient()
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log(event);
+
+  const result = await dynamoDBClient
+  .query({
+    TableName: todosTable,
+    IndexName: indexName,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': '123'
+    }
+  })
+  .promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      items: result.Items
+    })
+  }
 }
